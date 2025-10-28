59e71ca8dd86e2cbfcf16bfb1d9fc080bea19ae6
fortuneCodeHub <fortunenwohiri@gmail.com>
2025-10-28 22:34:30 +0100

    seo update

--- src/app/(landing)/access/page.jsx
+++ src/app/(landing)/access/page.jsx
@@ -96,7 +96,7 @@ const Access = () => {
         <div className="text-center mb-8">
             <Link href="/" className="relative text-2xl font-bold poppins-bold-italic">
                 <span className="text-[#0EA5A4]">Yield</span>
-                <span className="text-[#0F172A]">Witness</span>
+                <span className="text-[#0F172A]">Invest</span>
                 <span className="absolute bottom-0 left-0 w-full h-[3px] bg-[#0EA5A4]"></span>
             </Link>
 
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -88,13 +88,13 @@ export default function RootLayout({ children }) {
       <head>
         {/* Google tag (gtag.js) */}
         <Script
-          async
           src="https://www.googletagmanager.com/gtag/js?id=G-H1QYN3T4M0"
+          strategy="afterInteractive"
         />
-        <Script id="google-analytics">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+            function gtag(){window.dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-H1QYN3T4M0', {
               page_path: window.location.pathname,
