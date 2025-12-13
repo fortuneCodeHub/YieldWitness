1e34e9957aeadaa38d675b6b14edf6f199f54cec
fortuneCodeHub <fortunenwohiri@gmail.com>
2025-12-13 10:53:06 +0100

    New commit

--- src/app/api/auth/posts/photography/route.js
+++ src/app/api/auth/posts/photography/route.js
@@ -2,54 +2,6 @@ import Post from "@/models/post";
 import { connectToDB } from "@/utils/database";
 import { NextResponse } from "next/server";
 
-// export async function GET(request) {
-//   try {
-//     await connectToDB();
-
-//     const posts = await Post.find({
-//       category: "photography",
-//     }).sort({ createdAt: -1 });
-
-//     return new NextResponse(
-//       JSON.stringify({ success: true, posts }),
-//       {
-//         status: 200,
-//         headers: {
-//           "Access-Control-Allow-Origin": "http://localhost:3001",
-//           "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-//           "Access-Control-Allow-Headers": "Content-Type, Authorization",
-//         }
-//       }
-//     );
-
-//   } catch (err) {
-//     console.error("Fetch Posts API Error:", err);
-
-//     return new NextResponse(
-//       JSON.stringify({ error: "Server error" }),
-//       {
-//         status: 500,
-//         headers: {
-//           "Access-Control-Allow-Origin": "http://localhost:3001",
-//         }
-//       }
-//     );
-//   }
-// }
-
-// // Allow OPTIONS request for CORS preflight
-// export async function OPTIONS() {
-//   return new NextResponse(null, {
-//     status: 204,
-//     headers: {
-//       "Access-Control-Allow-Origin": "http://localhost:3001",
-//       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-//       "Access-Control-Allow-Headers": "Content-Type, Authorization",
-//     }
-//   });
-// }
-
-
 export async function GET(request) {
     const origin = request.headers.get("origin");
     const allowedOrigins = [
@@ -58,7 +10,8 @@ export async function GET(request) {
         "http://localhost:3003",
         "http://localhost:3004",
         "http://localhost:3005",
-    //   "https://www.pixelmuseart.vercel.app",
+        "https://framealchemy.vercel.app/",
+        "https://www.framealchemy.vercel.app/",
     ];
   
     const CORS_HEADERS = {
@@ -71,7 +24,7 @@ export async function GET(request) {
     try {
       await connectToDB();
   
-      const posts = await Post.find()
+      const posts = await Post.find({ category: "photography" })
         .sort({ createdAt: -1 });
   
       return new NextResponse(JSON.stringify({ success: true, posts }), {
