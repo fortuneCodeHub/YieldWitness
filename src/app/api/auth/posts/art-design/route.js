677603ed68f36510348919232f1223f1259a9847
fortuneCodeHub <fortunenwohiri@gmail.com>
2025-12-13 11:02:11 +0100

    New commit

--- src/app/api/auth/posts/art-design/route.js
+++ src/app/api/auth/posts/art-design/route.js
@@ -2,53 +2,6 @@ import Post from "@/models/post";
 import { connectToDB } from "@/utils/database";
 import { NextResponse } from "next/server";
 
-// export async function GET(request) {
-//   try {
-//     await connectToDB();
-
-//     const posts = await Post.find({
-//       category: "art-design",
-//     }).sort({ createdAt: -1 });
-
-//     return new NextResponse(
-//       JSON.stringify({ success: true, posts }),
-//       {
-//         status: 200,
-//         headers: {
-//           "Access-Control-Allow-Origin": "https://pixelmuseart.vercel.app",
-//           "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-//           "Access-Control-Allow-Headers": "Content-Type, Authorization",
-//         }
-//       }
-//     );
-
-//   } catch (err) {
-//     console.error("Fetch Posts API Error:", err);
-
-//     return new NextResponse(
-//       JSON.stringify({ error: "Server error" }),
-//       {
-//         status: 500,
-//         headers: {
-//           "Access-Control-Allow-Origin": "https://pixelmuseart.vercel.app",
-//         }
-//       }
-//     );
-//   }
-// }
-
-// // Allow OPTIONS request for CORS preflight
-// export async function OPTIONS() {
-//   return new NextResponse(null, {
-//     status: 204,
-//     headers: {
-//       "Access-Control-Allow-Origin": "https://pixelmuseart.vercel.app",
-//       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-//       "Access-Control-Allow-Headers": "Content-Type, Authorization",
-//     }
-//   });
-// }
-
 
 export async function GET(request) {
     const origin = request.headers.get("origin");
